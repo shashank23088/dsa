de614afb5ebc69d1e972aef11f363ae1de4b8675
Shashank G. Sharma <shashank23088@iiitd.ac.in>
2024-06-05 16:54:28 +0530

    done patterns 10 ques

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,16 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+void doSomething(string c, int n) {
+
+    for (int i = 0; i < n; i++) {
+        // for (int j1 = 0; j1 < n - i - 1; j1++) {
+        //     cout << " ";
+        // }
+        for (int j2 = 0; j2 < i + 1; j2++) {
+            cout << "*";
+        }
+        // for (int j1 = 0; j1 < n - i - 1; j1++) {
+        //     cout << " ";
+        // }
+        cout << endl;
+    }
+
+    for (int i = n; i > 0; i--) {
+        // for (int j1 = 0; j1 < n - i; j1++) {
+        //     cout << " ";
+        // }
+        for (int j2 = i - 1; j2 > 0; j2--) {
+            cout << "*";
+        }
+        // for (int j1 = 0; j1 < n - i; j1++) {
+        //     cout << " ";
+        // }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+    string c = "*";
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        doSomething(c, n);
     }
-    cout << endl;
+
+    return 0;
 }
