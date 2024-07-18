07b327b7796ae3e8db23d6a57e549d9343524ac9
Shashank G. Sharma <shashank23088@iiitd.ac.in>
2024-07-18 23:46:27 +0530

    added solution to delete node given in O(1)

---
+++ ll/delete_given_node.cpp
@@ -0,0 +1,12 @@
+    void deleteNode(ListNode* node) {
+	if (node -> next == NULL) {
+	    node = NULL;
+	}
+
+        node -> val  = node -> next -> val;
+        node -> next = node -> next ->  next;
+    }
+
+    // O(1) solution
+
+   
